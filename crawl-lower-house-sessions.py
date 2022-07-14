2fc1ef429a37e180dd5f10b1b947779f84043bcf
RePierre <petru.rebeja@gmail.com>
2022-07-14 09:38:12 +0300

    Added UrlBuilder class for building request URLs

--- crawl-lower-house-sessions.py
+++ crawl-lower-house-sessions.py
@@ -6,11 +6,61 @@ import datetime
 import logging
 
 
+class UrlBuilder:
+    """Builds URLs for requests."""
+
+    BaseUrl = "http://www.cdep.ro"
+    YearUrlTemplate = "/pls/steno/steno2015.calendar?cam=2&an={year}&idl=1"
+    SessionUrlTemplate = "/pls/steno/steno2015.data?cam=2&dat={date}&idl=1"
+
+    def __init__(self):
+        """Create a new instance of the class."""
+        pass
+
+    @classmethod
+    def build_URL_for_year(self, year):
+        """Build URL for retrieving the calendar of sessions.
+
+        Parameters
+        ----------
+        year: int, required
+            The year for which to build the URL.
+
+        Returns
+        -------
+        URL: str
+            The URL of the page containing the calendar of sessions for the specified year.
+        """
+        path_and_query = UrlBuilder.YearUrlTemplate.format(year=year)
+        return UrlBuilder.BaseUrl + path_and_query
+
+    def build_URL_for_session(self, session_date):
+        """Build URL for a session that took place on the specified date.
+
+        Parameters
+        ----------
+        session_date: datetime.date, required
+            The date of the session.
+
+        Returns
+        -------
+        URL: str
+            The URL of the session transcript for the provided date.
+        """
+        date_string = session_date.strftime("%Y%m%d")
+        path_and_query = UrlBuilder.SessionUrlTemplate.format(date=date_string)
+        return UrlBuilder.BaseUrl + path_and_query
+
+
 def main(args):
-    """Crawls session transcripts."""
+    """Crawl session transcripts."""
+    url_builder = UrlBuilder()
     if args.date:
         logging.info("Crawling session transcript for date {}.".format(
             args.date))
+        session_url = url_builder.build_URL_for_session(args.date)
+        logging.info(
+            "The URL of the session transcript is {}.".format(session_url))
     else:
         for year in args.years:
             logging.info(
