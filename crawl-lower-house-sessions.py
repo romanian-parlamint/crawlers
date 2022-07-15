631fc298f387186aa16e9889301a0b26cf65d6b2
RePierre <petru.rebeja@gmail.com>
2022-07-15 15:03:41 +0300

    Created class for crawling session URLs

--- crawl-lower-house-sessions.py
+++ crawl-lower-house-sessions.py
@@ -4,6 +4,9 @@ from argparse import ArgumentParser
 from argparse import ArgumentTypeError
 import datetime
 import logging
+import requests
+import re
+from lxml import html
 
 
 class UrlBuilder:
@@ -17,7 +20,21 @@ class UrlBuilder:
         """Create a new instance of the class."""
         pass
 
-    @classmethod
+    def build_full_URL(self, path_and_query):
+        """Build a full URL by appending base URL to path and query.
+
+        Parameters
+        ----------
+        path_and_query: str, required
+            The path and query parts of the URL.
+
+        Returns
+        -------
+        URL: str,
+            The full URL.
+        """
+        return UrlBuilder.BaseUrl + path_and_query
+
     def build_URL_for_year(self, year):
         """Build URL for retrieving the calendar of sessions.
 
@@ -32,7 +49,7 @@ class UrlBuilder:
             The URL of the page containing the calendar of sessions for the specified year.
         """
         path_and_query = UrlBuilder.YearUrlTemplate.format(year=year)
-        return UrlBuilder.BaseUrl + path_and_query
+        return self.build_full_URL(path_and_query)
 
     def build_URL_for_session(self, session_date):
         """Build URL for a session that took place on the specified date.
@@ -49,7 +66,45 @@ class UrlBuilder:
         """
         date_string = session_date.strftime("%Y%m%d")
         path_and_query = UrlBuilder.SessionUrlTemplate.format(date=date_string)
-        return UrlBuilder.BaseUrl + path_and_query
+        return self.build_full_URL(path_and_query)
+
+
+class SessionUrlsCrawler:
+    """Crawl session URLs for a specific year."""
+
+    SessionUrlRegex = r"\/pls\/steno\/steno2015.data\?cam=2&dat=(?P<date>\d{8})&idl=1"
+
+    def __init__(self):
+        """Create a new instance of session URLs crawler."""
+        self.__url_builder = UrlBuilder()
+
+    def crawl(self, year):
+        """Crawl session URLs.
+
+        Parameters
+        ----------
+        year: int, required
+            The year for which to crawl session URLs.
+
+        Returns
+        -------
+        dates_and_urls: iterable of tuples of (datetime, str)
+            The collection of session dates and their URLS for the specified year.
+        """
+        url = self.__url_builder.build_URL_for_year(year)
+        response = requests.get(url)
+        html_root = html.fromstring(response.content)
+        result = []
+        for a in html_root.iterdescendants(tag='a'):
+            href = a.get('href')
+            match = re.search(SessionUrlsCrawler.SessionUrlRegex, href,
+                              re.MULTILINE)
+            if match:
+                date_str = match.group('date')
+                session_date = datetime.datetime.strptime(date_str, "%Y%m%d")
+                session_url = self.__url_builder.build_full_URL(href)
+                result.append((session_date, session_url))
+        return result
 
 
 def main(args):
@@ -62,9 +117,12 @@ def main(args):
         logging.info(
             "The URL of the session transcript is {}.".format(session_url))
     else:
+        crawler = SessionUrlsCrawler()
         for year in args.years:
             logging.info(
                 "Crawling session transcripts for year {}.".format(year))
+            for date, url in crawler.crawl(year):
+                print(date, url)
 
 
 def valid_year(year):
