f54aac670a3f8b3f96cff4f3b21cae2a71957328
RePierre <petru.rebeja@gmail.com>
2022-08-03 23:28:30 +0300

    Refactored parsing of speaker info into multiple methods

--- core/crawling/speakers.py
+++ core/crawling/speakers.py
@@ -25,40 +25,112 @@ class SpeakerInfoParser:
         speaker: dict
             The parsed speaker information.
         """
+        name_element = self.__get_name_element(element)
+        if name_element is None:
+            return None
+
+        name_with_prefix = name_element.text_content()
+
+        return {
+            'text': element.text_content().strip(),
+            'full_name': self.__parse_full_name(name_with_prefix),
+            'profile_url': self.__parse_profile_url(element),
+            'sex': self.__parse_speaker_sex(name_with_prefix),
+            'annotation': self.__parse_annotation(element)
+        }
+
+    def __get_name_element(self, element):
+        """Get the name element from the provided element, if present.
+
+        Parameters
+        ----------
+        element: etree.Element, required
+            The parent element from which to extract name element.
+
+        Returns
+        -------
+        name_element: etree.Element
+            The name element if present; None otherwise.
+        """
         fonts = [f for f in element.iterdescendants(tag='font')]
         if len(fonts) == 0:
             # The element doesn't contain the name of a speaker
             return None
 
-        text = element.text_content().strip()
+        return fonts[0]
 
-        # Parse full name of the speaker
-        name_element_text = fonts[0].text_content()
-        sex = 'M' if name_element_text.startswith("Domnul") else 'F'
-        full_name = re.sub(r'domnul|doamna|(\(.+\)*)?:', '', name_element_text,
+    def __parse_speaker_sex(self, name_with_prefix):
+        """Parse speaker sex from the text containing name and prefix.
+
+        Parameters
+        ----------
+        name_with_prefix: str, required
+            The name and prefix of the speaker.
+
+        Returns
+        -------
+        sex: str
+            The sex of the speaker; 'M' if male, 'F' if female.
+        """
+        return 'M' if name_with_prefix.lower().startswith("domnul") else 'F'
+
+    def __parse_full_name(self, name_with_prefix):
+        """Parse the full name of the speaker from the name with prefix.
+
+        Parameters
+        ----------
+        name_with_prefix: str, required
+            The name and prefix of the speaker.
+
+        Returns
+        -------
+        full_name: str
+            The full name of the speaker.
+        """
+        full_name = re.sub(r'domnul|doamna|(\(.+\)*)?:', '', name_with_prefix,
                            0, re.MULTILINE | re.IGNORECASE)
-        full_name = full_name.strip()
+        return full_name.strip()
 
-        # Get profile URL if present
+    def __parse_profile_url(self, element):
+        """Parse the profile URL of the speaker if present.
+
+        Parameters
+        ----------
+        element: etree.Element, required
+             The element containing speaker information.
+
+        Returns
+        -------
+        profile_url: str
+            The full URL of the speaker profile if present; otherwise None.
+        """
         profile_url = None
         anchors = [
             a for a in element.iterdescendants(tag='a')
             if a.get('target') == "PARLAMENTARI"
         ]
+
         if len(anchors) > 0:
             profile_url = anchors[0].get('href')
             profile_url = self.__ulr_builder.build_full_URL(profile_url)
 
-        # Get annotation if present
+        return profile_url
+
+    def __parse_annotation(self, element):
+        """Parse the annotation from speaker info element if present.
+
+        Parameters
+        ----------
+        element: etree.Element, required
+             The element containing speaker information.
+
+        Returns
+        -------
+        annotation: str
+            The annotation beside the speaker name if present; otherwise None.
+        """
         annotation = None
         comments = [i for i in element.iterdescendants(tag='i')]
         if len(comments) > 0:
             annotation = comments[0].text_content()
-
-        return {
-            'text': text,
-            'full_name': full_name,
-            'profile_url': profile_url,
-            'sex': sex,
-            'annotation': annotation
-        }
+        return annotation
