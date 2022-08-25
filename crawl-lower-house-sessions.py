99eb8d6950d20507f5b22e2299e3e13c1aad1e08
RePierre <petru.rebeja@gmail.com>
2022-08-25 06:38:23 +0300

    Moved function configure_logging to loggingutils module

--- crawl-lower-house-sessions.py
+++ crawl-lower-house-sessions.py
@@ -10,6 +10,7 @@ from core.crawling.summary import SessionSummaryCrawler
 from core.crawling.session import SessionTranscriptCrawler
 import json
 from pathlib import Path
+from utils.loggingutils import configure_logging
 
 
 def get_parsed_sessions(transcripts_dir):
@@ -135,33 +136,6 @@ def valid_year(year):
         raise ArgumentTypeError(
             "Year must be betweer 2000 and {} inclusive.".format(max_year))
     return year
-
-
-def configure_logging(log_level, log_file):
-    """Configure logging.
-
-    Parameters
-    ----------
-    log_level: str, required
-        The level of messages to display in logs.
-    log_file: str, required
-        The path of the log file.
-    """
-    if log_file is not None:
-        logging.basicConfig(
-            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-            filename=log_file,
-            filemode='w',
-            level=log_level)
-        console = logging.StreamHandler()
-        console.setLevel(log_level)
-        formatter = logging.Formatter(
-            '%(name)-12s: %(levelname)-8s %(message)s')
-        console.setFormatter(formatter)
-        logging.getLogger('').addHandler(console)
-    else:
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                            level=log_level)
 
 
 def parse_arguments():
