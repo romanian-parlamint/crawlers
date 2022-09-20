65595772d88a21aa4350019bd5dd8eca7ffb021e
RePierre <petru.rebeja@gmail.com>
2022-09-20 20:24:48 +0300

    Added possibility to append to existing file when saving data

--- framework/utils/dataframeutils.py
+++ framework/utils/dataframeutils.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """Utility functions for pandas.DataFrame objects."""
 from pathlib import Path
+import pandas as pd
 import csv
 
 
-def save_data_frame(data_frame, file_name):
+def save_data_frame(data_frame: pd.DataFrame,
+                    file_name: str,
+                    append: bool = False):
     """Save the provided data frame into specified CSV file.
 
     Parameters
@@ -13,8 +16,12 @@ def save_data_frame(data_frame, file_name):
         The data frame to save.
     file_name: str, required
         The path of the CSV file where to save the data frame.
+    append: bool, optional
+        If set to True the data will be appended to existing CSV file;
+        otherwise the file will be overwritten.
     """
     output_file = Path(file_name)
     if not output_file.parent.exists():
         output_file.parent.mkdir(parents=True, exist_ok=True)
-    data_frame.to_csv(file_name, quoting=csv.QUOTE_NONNUMERIC)
+    write_mode = 'a' if append else 'w'
+    data_frame.to_csv(file_name, quoting=csv.QUOTE_NONNUMERIC, mode=write_mode)
