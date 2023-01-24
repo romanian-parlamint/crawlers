e001b397c2ed0b338803da671da65be94359f19c
RePierre <petru.rebeja@gmail.com>
2023-01-24 14:50:32 +0200

    Added class for manipulating persons in corpus root file

--- framework/core/conversion/rootxmlcreation.py
+++ framework/core/conversion/rootxmlcreation.py
@@ -196,3 +196,91 @@ class OrganizationsListManipulator:
                 event_end) if event_end is not None else None
             yield ("#{}".format(term_id), term_start, term_end)
 
+
+class PersonListManipulator:
+    """Hadles updates and queries on the `listPerson` element contents."""
+
+    def __init__(self, xml_root: etree.Element):
+        """Create a new instance of the class.
+
+        Parameters
+        ----------
+        xml_root: etree.Element, required
+            The root node of the corpus root file.
+        """
+        self.__xml_root = xml_root
+        self.__persons_list = next(
+            xml_root.iterdescendants(tag=XmlElements.listPerson))
+
+    def add_or_update_person(self, person_id: str,
+                             legislative_term: Tuple[str, date, date]):
+        """Add or update person.
+
+        Parameters
+        ----------
+        person_id: str, required
+            The id of the person to add or update.
+        legislative_term: tuple of (str, date, date), required
+            The legislative term as a tuple of (term id, start date, end date) in which the person appears.
+        """
+        person_id = person_id.replace('#', '')
+        person = self.__get_person(person_id)
+        if person is None:
+            person = self.__create_person(person_id)
+        self.__update_affiliation(person, legislative_term)
+
+    def __update_affiliation(self, person: etree.Element,
+                             legislative_term: Tuple[str, date, date]):
+        """Add the legislative term to the affiliation of the person if it doesn't exist.
+
+        Parameters
+        ----------
+        person: etree.Element, required
+            The person element to update.
+        legislative_term: tuple of (str, date, date), required
+            The id, start date, and end date of the legislative term.
+        """
+        term_id, start_date, end_date = legislative_term
+        for affiliation in person.iterdescendants(tag=XmlElements.affiliation):
+            if affiliation.get(XmlAttributes.ana) == term_id:
+                # Affiliation already exists; nothing to do.
+                return
+
+        affiliation = etree.SubElement(person, XmlElements.affiliation)
+
+    def __create_person(self, person_id: str) -> etree.Element:
+        """Create a person element with the provided info.
+
+        Parameters
+        ----------
+        person_id: str, required
+            The id of the person element.
+
+        Returns
+        -------
+        person: etree.Element
+            The person element.
+        """
+        person = etree.SubElement(self.__persons_list, XmlElements.person)
+        person.set(XmlAttributes.xml_id, person_id)
+        return person
+
+    def __get_person(self, person_id: str) -> etree.Element:
+        """Get the person if person exists in persons list.
+
+        Parameters
+        ----------
+        person_id: str, required
+            The id of the person to search for.
+
+        Returns
+        -------
+        person: etree.Element
+            The person element if it exists; None otherwise.
+        """
+        for person in self.__persons_list.iterdescendants(
+                tag=XmlElements.person):
+            if person.get(XmlAttributes.xml_id) == person_id:
+                return person
+
+        return None
