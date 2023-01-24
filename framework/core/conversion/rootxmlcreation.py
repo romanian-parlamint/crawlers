88ed1876d5350cc0a738710300e61f42e06bb0c8
RePierre <petru.rebeja@gmail.com>
2023-01-24 14:48:45 +0200

    Added class for manipulating organizations in corpus root file

--- framework/core/conversion/rootxmlcreation.py
+++ framework/core/conversion/rootxmlcreation.py
@@ -114,3 +114,85 @@ class SessionSpeakersReader(XmlDataManipulator):
             ids.add(speaker_id)
         return list(ids)
 
+
+class OrganizationsListManipulator:
+    """Handles queries on the `listOrg` element contents."""
+
+    def __init__(self, xml_root: etree.Element):
+        """Create a new instance of the class.
+
+        Parameters
+        ----------
+        xml_root: etree.Element, required
+            The root node of the corpus root file.
+        """
+        self.__xml_root = xml_root
+        self.__parliament_org = None
+        self.__legislative_terms = None
+
+    @property
+    def parliament(self) -> etree.Element:
+        """Get the parliament organization.
+
+        Returns
+        -------
+        parliament: etree.Element,
+            The organization with role==parliament.
+        """ ""
+        if self.__parliament_org is None:
+            for org in self.__xml_root.iterdescendants(tag=XmlElements.org):
+                if org.get(XmlAttributes.role) == "parliament":
+                    self.__parliament_org = org
+        return self.__parliament_org
+
+    @property
+    def legislative_terms(self) -> List[Tuple[str, date, date]]:
+        """Get the legislative terms of the Parliament from the corpus root file.
+
+        Returns
+        -------
+        legislative_terms: list of (str, date, date) tuples
+            The legislative terms as tuples of (id, start date, end date).
+        """
+        if self.__legislative_terms is None:
+            self.__legislative_terms = list(self.__load_legislative_terms())
+        return self.__legislative_terms
+
+    def get_legislative_term(
+            self, session_date: date) -> Tuple[str, str, date, date]:
+        """Get the legislative term for the specified session date.
+
+        Returns
+        -------
+        (term_id, org_id, start_date, end_date): tuple of (str, date, date)
+            The tuple containing the term id, organization id, start date and end date
+            of the legislative term if found; otherwise (None, None, None, None).
+        """
+        for term_id, term_start, term_end in self.legislative_terms:
+            end_date = date.max if term_end is None else term_end
+            if term_start <= session_date <= end_date:
+                return (term_id, term_start, term_end)
+
+        logging.error("Could not find legislative term for session date %s.",
+                      session_date)
+        return (None, None, None)
+
+    def __load_legislative_terms(
+            self) -> Generator[Tuple[str, date, date], None, None]:
+        """Load legislative terms from the root file.
+
+        Returns
+        legislative_terms: generator of (str, date, date) tuples
+            The list of legislative terms as tuples of (term id, start date, end date).
+        """
+        parliament = self.parliament
+
+        for event in parliament.iterdescendants(tag=XmlElements.event):
+            term_id = event.get(XmlAttributes.xml_id)
+            term_start = date.fromisoformat(
+                event.get(XmlAttributes.event_start))
+            event_end = event.get(XmlAttributes.event_end)
+            term_end = date.fromisoformat(
+                event_end) if event_end is not None else None
+            yield ("#{}".format(term_id), term_start, term_end)
+
