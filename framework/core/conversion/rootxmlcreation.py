9f542ea5a6b470f2a30818d85b3bc5f9e1a80cc3
RePierre <petru.rebeja@gmail.com>
2023-01-24 14:47:33 +0200

    Added class to read session speakers

--- framework/core/conversion/rootxmlcreation.py
+++ framework/core/conversion/rootxmlcreation.py
@@ -70,3 +70,47 @@ class RootCorpusFileBuilder(XmlDataManipulator):
         provider = SessionStatsReader(component_path)
         writer = CorpusStatsWriter(self.xml_root, provider)
         writer.update_statistics()
+
+
+class SessionSpeakersReader(XmlDataManipulator):
+    """Read session speakers."""
+
+    def __init__(self, file_path: str):
+        """Create a new instance of the class.
+
+        Parameters
+        ----------
+        file_path: str, required
+            The path of the XML file containing session transcript.
+        """
+        XmlDataManipulator.__init__(self, file_path)
+        self.__session_date = None
+
+    @property
+    def session_date(self):
+        """Get the session date."""
+        if self.__session_date is None:
+            for date_elem in self.xml_root.iterdescendants(
+                    tag=XmlElements.date):
+                if date_elem.getparent().tag != XmlElements.bibl:
+                    continue
+                date = datetime.fromisoformat(date_elem.get(
+                    XmlAttributes.when))
+                self.__session_date = date.date()
+
+        return self.__session_date
+
+    def get_speaker_ids(self) -> List[str]:
+        """Read speaked ids from the session transcript.
+
+        Returns
+        -------
+        speaker_ids:  list of str
+            The list of unique speaker ids.
+        """
+        ids = set()
+        for utterance in self.xml_root.iterdescendants(tag=XmlElements.u):
+            speaker_id = utterance.get(XmlAttributes.who)
+            ids.add(speaker_id)
+        return list(ids)
+
