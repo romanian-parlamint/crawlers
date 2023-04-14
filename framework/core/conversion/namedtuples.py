5de78a6fd04b353f76515b65681e0634ede55778
RePierre <petru.rebeja@gmail.com>
2023-04-14 20:13:38 +0300

    Removed corpus building scripts from repository

--- framework/core/conversion/namedtuples.py
+++
@@ -1,7 +0,0 @@
-"""Defines named tuples."""
-from collections import namedtuple
-
-Event = namedtuple('Event', ['org_id', 'event_id', 'start_date', 'end_date'])
-
-PersonalInformation = namedtuple(
-    'PersonalInformation', ["first_name", "last_name", "sex", "profile_image"])
